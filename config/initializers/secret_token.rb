d439b90075e64db1d8402af596260b095e330835
Taco <taco@fourdesire.com>
2012-08-21 17:43:42 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModelApp::Application.config.secret_token = '703ea8cacda2873d002efc6b50b79333852983db0a0c7ab153f7ddf0c333379edcdf908f3ba075235369f3f0ca90bbf306ec051894488411ff87b4c6c31825c8'
